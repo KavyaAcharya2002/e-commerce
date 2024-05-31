e8003a5beb559f64fbd7a610edf624083c880537
Kavya Acharya <210926@sdmcujire.in>
2024-05-31 12:10:15 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='about'>
+      <h1>About Page </h1>
+      <h4>Be it the pop-culture portrayal or the never-ending sales, no one leaves a store with just a loaf of bread. Money may not be able to buy happiness, but it can surely get you started on fashion.</h4>
+        <img src="https://cdn.shopify.com/s/files/1/0070/7032/files/allbirds-2.png?v=1647466716"width='100%' height="450px">
+        </img> 
+      
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='pro'>
+      <h1>WELCOME TO OUR PAGE</h1>
+      <img src="https://wallpaperaccess.com/full/2484120.jpg">
+
+      </img>
+      
+    </div>
+  );
+}
+
+export default Home;
